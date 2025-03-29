a667a41270700ffc3884aa6c93c9226c9b7f7339
Umang Saini <sainiumang505@gmail.com>
2025-03-29 10:18:37 +0530

    Navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -7,15 +7,18 @@ import {
     SignedIn,
     SignedOut,
     UserButton,
+
+
 } from '@clerk/nextjs'
 
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center text-white bg-purple-700">
-            <ClerkProvider>
+        <ClerkProvider>
+            <nav className="flex h-18 justify-between items-center text-white bg-purple-700">
+
                 <div className="logo flex items-center justify-center">
-                    <img className='flex h-16 rounded-full m-3 px-1' src='https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-student-glyph-black-icon-png-image_691145.jpg' />
+                    <img className='flex rounded-full m-3 px-1 h-12' src='https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-student-glyph-black-icon-png-image_691145.jpg' />
                     <ul className='flex gap-4 text-xl font-semibold'>
                         <Link href="/"><li>Home </li></Link>
                         <Link href="/"><li>About</li></Link>
@@ -27,27 +30,29 @@ const Navbar = () => {
                 <div className="flex gap-3 m-2">
                     <SignedOut>
                         <SignInButton >
-                            <button className="login text-black bg-gray-200 p-4 rounded-lg font-bold hover:bg-gray-300 transition duration-200 m-2">
+                            <button className="login text-black bg-gray-200 p-3 rounded-lg font-bold hover:bg-gray-300 transition duration-200 m-2">
                                 Log in
                             </button>
                         </SignInButton >
                         <SignUpButton >
-                            <button className="signup bg-gray-900 text-white font-bold p-3 rounded-full hover:bg-gray-800 transition duration-200 mb-1 me-1 mt-0.5">
+                            <button className="signup bg-gray-900 text-white font-bold p-2 rounded-full hover:bg-gray-800 transition duration-200 mb-1 me-1 mt-0.5">
                                 Sign up free
                             </button>
                         </SignUpButton>
                     </SignedOut>
                     <SignedIn>
 
-                        <div className='flex px-2 m-1 items-center'>
+                        <div className='flex px-2 h-5 m-1 items-center'>
+                            <p className='text-white font-bold m-2 text-xl'>Welcome!</p>
                             <UserButton />
                         </div>
 
 
                     </SignedIn>
                 </div>
-            </ClerkProvider>
-        </nav >
+
+            </nav >
+        </ClerkProvider>
 
     )
 }
